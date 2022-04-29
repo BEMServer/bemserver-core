aa1deaccac84d89fdc43baff100add195a49bcd5
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-04-29 10:30:22 +0200

    Add DB naming convention

--- bemserver_core/database.py
+++ bemserver_core/database.py
@@ -5,6 +5,16 @@ import sqlalchemy as sqla
 from sqlalchemy.orm import sessionmaker, scoped_session, declarative_base
 
 
+# https://alembic.sqlalchemy.org/en/latest/naming.html
+NAMING_CONVENTION = {
+    "ix": "ix_%(column_0_label)s",
+    "uq": "uq_%(table_name)s_%(column_0_name)s",
+    "ck": "ck_%(table_name)s_%(constraint_name)s",
+    "fk": "fk_%(table_name)s_%(column_0_name)s_%(referred_table_name)s",
+    "pk": "pk_%(table_name)s",
+}
+
+
 class Base:
     """Custom base class"""
 
@@ -98,7 +108,8 @@ class Base:
 
 SESSION_FACTORY = sessionmaker()
 DB_SESSION = scoped_session(SESSION_FACTORY)
-Base = declarative_base(cls=Base)
+metadata = sqla.MetaData(naming_convention=NAMING_CONVENTION)
+Base = declarative_base(metadata=metadata, cls=Base)
 
 
 class DBConnection:
