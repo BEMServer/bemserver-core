b9fe4d31e617011eda040203cce2046a78bb5134
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-06-07 11:24:20 +0200

    Add TimeseriesDataIO.delete

--- tests/input_output/test_timeseries_data_io.py
+++ tests/input_output/test_timeseries_data_io.py
@@ -666,3 +666,112 @@ class TestTimeseriesDataCSVIO:
                 tsdcsvio.export_csv_bucket(
                     start_dt, end_dt, ts_l, dummy_ds_id, "1 day", campaign=campaign
                 )
+
+    @pytest.mark.parametrize("timeseries", (5,), indirect=True)
+    @pytest.mark.parametrize("for_campaign", (True, False))
+    def test_timeseries_data_io_delete_as_admin(
+        self, users, timeseries, campaigns, for_campaign
+    ):
+        admin_user = users[0]
+        assert admin_user.is_admin
+        ts_0 = timeseries[0]
+        ts_2 = timeseries[2]
+        campaign = campaigns[0] if for_campaign else None
+
+        start_dt = dt.datetime(2020, 1, 1, tzinfo=dt.timezone.utc)
+        end_dt = start_dt + dt.timedelta(hours=3)
+
+        # Create DB data
+        with OpenBar():
+            ds_1 = TimeseriesDataState.get(name="Raw").first()
+            tsbds_0 = ts_0.get_timeseries_by_data_state(ds_1)
+            for i in range(3):
+                timestamp = start_dt + dt.timedelta(hours=i)
+                db.session.add(
+                    TimeseriesData(
+                        timestamp=timestamp,
+                        timeseries_by_data_state_id=tsbds_0.id,
+                        value=i,
+                    )
+                )
+            db.session.commit()
+
+            assert db.session.query(TimeseriesData).all()
+
+        if for_campaign:
+            ts_l = (ts_0.name, ts_2.name)
+        else:
+            ts_l = (ts_0.id, ts_2.id)
+
+        with CurrentUser(admin_user):
+            tsdcsvio.delete(start_dt, end_dt, ts_l, ds_1.id, campaign)
+            assert not db.session.query(TimeseriesData).all()
+
+    @pytest.mark.parametrize("timeseries", (4,), indirect=True)
+    @pytest.mark.usefixtures("users_by_user_groups")
+    @pytest.mark.usefixtures("user_groups_by_campaigns")
+    @pytest.mark.usefixtures("user_groups_by_campaign_scopes")
+    @pytest.mark.parametrize("for_campaign", (True, False))
+    def test_timeseries_data_io_delete_as_user(
+        self, users, timeseries, campaigns, for_campaign
+    ):
+        user_1 = users[1]
+        assert not user_1.is_admin
+        ts_0 = timeseries[0]
+        ts_1 = timeseries[1]
+        ts_2 = timeseries[2]
+        ts_3 = timeseries[3]
+
+        start_dt = dt.datetime(2020, 1, 1, tzinfo=dt.timezone.utc)
+        end_dt = start_dt + dt.timedelta(hours=3)
+
+        # Create DB data
+        with OpenBar():
+            ds_1 = TimeseriesDataState.get(name="Raw").first()
+            tsbds_0 = ts_0.get_timeseries_by_data_state(ds_1)
+            tsbds_1 = ts_1.get_timeseries_by_data_state(ds_1)
+            for i in range(3):
+                timestamp = start_dt + dt.timedelta(hours=i)
+                db.session.add(
+                    TimeseriesData(
+                        timestamp=timestamp,
+                        timeseries_by_data_state_id=tsbds_0.id,
+                        value=i,
+                    )
+                )
+                db.session.add(
+                    TimeseriesData(
+                        timestamp=timestamp,
+                        timeseries_by_data_state_id=tsbds_1.id,
+                        value=i,
+                    )
+                )
+            db.session.commit()
+
+            assert db.session.query(TimeseriesData).all()
+
+        if for_campaign:
+            campaign = campaigns[0]
+            ts_l = (ts_0.name, ts_2.name)
+        else:
+            campaign = None
+            ts_l = (ts_0.id, ts_2.id)
+
+        with CurrentUser(user_1):
+            with pytest.raises(BEMServerAuthorizationError):
+                tsdcsvio.delete(start_dt, end_dt, ts_l, ds_1.id, campaign)
+
+        if for_campaign:
+            campaign = campaigns[1]
+            ts_l = (ts_1.name, ts_3.name)
+        else:
+            campaign = None
+            ts_l = (ts_1.id, ts_3.id)
+
+        with CurrentUser(user_1):
+            tsdcsvio.delete(start_dt, end_dt, ts_l, ds_1.id, campaign)
+            assert (
+                not db.session.query(TimeseriesData)
+                .filter_by(timeseries_by_data_state_id=tsbds_1.id)
+                .all()
+            )
