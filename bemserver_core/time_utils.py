8c3ae8ac8586da910a978c36a300fbdc009152cc
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2022-11-09 15:30:08 +0100

    Add time_utils.ceil

--- bemserver_core/time_utils.py
+++ bemserver_core/time_utils.py
@@ -3,6 +3,7 @@ import datetime as dt
 from zoneinfo import ZoneInfo
 
 import pandas as pd
+from pandas.tseries.offsets import DateOffset
 
 from bemserver_core.exceptions import BEMServerCorePeriodError
 
@@ -59,6 +60,58 @@ def floor(datetime, period, period_multiplier=1):
         return pd.Timestamp(
             datetime.year, datetime.month, datetime.day, tzinfo=tz
         ) - dt.timedelta(days=datetime.weekday())
+
+    raise BEMServerCorePeriodError(f'Invalid period: "{period}"')
+
+
+def ceil(datetime, period, period_multiplier=1):
+    """Ceil datetime to a given time period
+
+    :param str period: Period in
+        ["second", "minute", "hour", "day", "week", "month", "year"]
+    :param int period_multiplier: Period multiplier.
+
+    Period multipliers are only allowed for fixed size periods (second, minute, hour).
+    If using multipliers, note that the origin of the period is on Jan 1st 1970.
+    This can lead to surprising results if the multiplied period is not a divider of
+    above period. E.g. "4 hours" starts at midnight, while "5 hours" may not.
+    """
+    if period in FIXED_SIZE_PERIODS:
+        pd_freq = f"{period_multiplier}{PANDAS_PERIOD_ALIASES[period]}"
+        return pd.Timestamp(datetime).ceil(pd_freq, ambiguous=datetime.fold)
+
+    if period_multiplier != 1:
+        raise BEMServerCorePeriodError(
+            "Period multipliers only allowed for fixed size periods"
+        )
+
+    tz = datetime.tzinfo
+
+    if period == "year":
+        ret = pd.Timestamp(datetime.year, 1, 1, tzinfo=tz)
+        if datetime != floor(datetime, period):
+            ret += DateOffset(years=1)
+        return ret
+    if period == "month":
+        ret = pd.Timestamp(datetime.year, datetime.month, 1, tzinfo=tz)
+        if datetime != floor(datetime, period):
+            ret += DateOffset(months=1)
+        return ret
+    if period == "day":
+        ret = pd.Timestamp(datetime.year, datetime.month, datetime.day, tzinfo=tz)
+        if datetime != floor(datetime, period):
+            ret += DateOffset(days=1)
+        return ret
+    if period == "week":
+        # Week: align on monday
+        # Note that timedelta arithmetics respect wall clock so subtracting days
+        # works even across DST
+        ret = pd.Timestamp(
+            datetime.year, datetime.month, datetime.day, tzinfo=tz
+        ) - dt.timedelta(days=datetime.weekday())
+        if datetime != floor(datetime, period):
+            ret += DateOffset(days=7)
+        return ret
 
     raise BEMServerCorePeriodError(f'Invalid period: "{period}"')
 
--- tests/test_time_utils.py
+++ tests/test_time_utils.py
@@ -3,14 +3,14 @@ from zoneinfo import ZoneInfo
 
 import pytest
 
-from bemserver_core.time_utils import floor
+from bemserver_core.time_utils import floor, ceil
 from bemserver_core.exceptions import BEMServerCorePeriodError
 
 
 PERIODS = ("second", "minute", "hour", "day", "week", "month", "year")
 
 
-class TestTimeUtils:
+class TestTimeUtilsFloor:
     @pytest.mark.parametrize("timezone", (dt.timezone.utc, ZoneInfo("Europe/Paris")))
     def test_floor(self, timezone):
         datetime = dt.datetime(2020, 3, 17, 5, 23, 54, 123, tzinfo=timezone)
@@ -58,3 +58,69 @@ class TestTimeUtils:
             match="Period multipliers only allowed for fixed size periods",
         ):
             floor(dt.datetime(2020, 1, 1), period, 2)
+
+
+class TestTimeUtilsCeil:
+    @pytest.mark.parametrize("timezone", (dt.timezone.utc, ZoneInfo("Europe/Paris")))
+    def test_ceil(self, timezone):
+        datetime = dt.datetime(2020, 3, 17, 5, 23, 54, 123, tzinfo=timezone)
+        assert ceil(datetime, "year") == dt.datetime(2021, 1, 1, tzinfo=timezone)
+        assert ceil(datetime, "month") == dt.datetime(2020, 4, 1, tzinfo=timezone)
+        assert ceil(datetime, "week") == dt.datetime(2020, 3, 23, tzinfo=timezone)
+        assert ceil(datetime, "week").weekday() == 0
+        assert ceil(datetime, "day") == dt.datetime(2020, 3, 18, tzinfo=timezone)
+        assert ceil(datetime, "hour") == dt.datetime(2020, 3, 17, 6, tzinfo=timezone)
+        assert ceil(datetime, "hour", 2) == dt.datetime(2020, 3, 17, 6, tzinfo=timezone)
+        assert ceil(datetime, "hour", 12) == dt.datetime(
+            2020, 3, 17, 12, tzinfo=timezone
+        )
+        assert ceil(datetime, "minute") == dt.datetime(
+            2020, 3, 17, 5, 24, tzinfo=timezone
+        )
+        assert ceil(datetime, "minute", 10) == dt.datetime(
+            2020, 3, 17, 5, 30, tzinfo=timezone
+        )
+        assert ceil(datetime, "minute", 15) == dt.datetime(
+            2020, 3, 17, 5, 30, tzinfo=timezone
+        )
+        assert ceil(datetime, "second") == dt.datetime(
+            2020, 3, 17, 5, 23, 55, tzinfo=timezone
+        )
+        assert ceil(datetime, "second", 4) == dt.datetime(
+            2020, 3, 17, 5, 23, 56, tzinfo=timezone
+        )
+        assert ceil(datetime, "second", 20) == dt.datetime(
+            2020, 3, 17, 5, 24, 0, tzinfo=timezone
+        )
+
+    @pytest.mark.parametrize("timezone", (dt.timezone.utc, ZoneInfo("Europe/Paris")))
+    def test_ceil_round_value(self, timezone):
+        datetime = dt.datetime(2018, 1, 1, 0, 0, 0, 0, tzinfo=timezone)
+        assert ceil(datetime, "year") == datetime
+        assert ceil(datetime, "month") == datetime
+        assert ceil(datetime, "week") == datetime
+        assert ceil(datetime, "week").weekday() == 0
+        assert ceil(datetime, "day") == datetime
+        assert ceil(datetime, "hour") == datetime
+        assert ceil(datetime, "hour", 2) == datetime
+        assert ceil(datetime, "hour", 12) == datetime
+        assert ceil(datetime, "minute") == datetime
+        assert ceil(datetime, "minute", 10) == datetime
+        assert ceil(datetime, "minute", 15) == datetime
+        assert ceil(datetime, "second") == datetime
+        assert ceil(datetime, "second", 4) == datetime
+        assert ceil(datetime, "second", 20) == datetime
+
+    def test_ceil_invalid_period(self):
+        with pytest.raises(
+            BEMServerCorePeriodError, match='Invalid period: "dummy period"'
+        ):
+            ceil(dt.datetime(2020, 1, 1), "dummy period")
+
+    @pytest.mark.parametrize("period", ("day", "week", "month", "year"))
+    def test_ceil_variable_size_with_multiplier(self, period):
+        with pytest.raises(
+            BEMServerCorePeriodError,
+            match="Period multipliers only allowed for fixed size periods",
+        ):
+            ceil(dt.datetime(2020, 1, 1), period, 2)
