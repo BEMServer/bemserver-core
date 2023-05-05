3eb95cbe1cd0cf01445edd8148ac84d4112fef36
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2023-05-05 11:24:03 +0200

    Bump version: 0.13.5 → 0.14.0

--- bemserver_core/__init__.py
+++ bemserver_core/__init__.py
@@ -13,7 +13,7 @@ from bemserver_core.process.weather import wdp
 from bemserver_core.exceptions import BEMServerCoreSettingsError
 
 
-__version__ = "0.13.5"
+__version__ = "0.14.0"
 
 
 class BEMServerCore:
