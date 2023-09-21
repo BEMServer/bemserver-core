13114c0a1786770a4f1c3560593445d39ee98b69
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2023-09-21 13:53:38 +0200

    Support Python 3.11

--- setup.py
+++ setup.py
@@ -23,6 +23,7 @@ setup(
         "Topic :: Scientific/Engineering",
         "Programming Language :: Python :: 3.9",
         "Programming Language :: Python :: 3.10",
+        "Programming Language :: Python :: 3.11",
         (
             "License :: OSI Approved :: "
             "GNU Affero General Public License v3 or later (AGPLv3+)"
