c2155d1429296bb2796836ea9fd40911f5fa341d
scls19fr <s.celles@gmail.com>
2024-04-08 14:22:10 +0200

    entrypoint uniformity

--- docs/deployment/bemserver-docker-deployment/bemserver-ui/entrypoint.sh
+++ docs/deployment/bemserver-docker-deployment/bemserver-ui/entrypoint.sh
@@ -3,10 +3,10 @@
 if [[ "$FLASK_ENV" == "production" ]]; then
     if [[ "$BEMSERVER_API_USE_SSL" ]]; then
         echo "Run app using gunicorn with SSL"
-        gunicorn --bind 0.0.0.0:5000 --certfile=/etc/ssl/server.crt --keyfile=/etc/ssl/server.key "app:create_app()"
+        gunicorn --bind 0.0.0.0:5001 --certfile=/etc/ssl/server.crt --keyfile=/etc/ssl/server.key "app:create_app()"
     else
         echo "Run app gunicorn without SSL"
-        gunicorn --bind 0.0.0.0:5000 "app:create_app()"
+        gunicorn --bind 0.0.0.0:5001 "app:create_app()"
     fi
 else
     echo "App is not running in production mode."
