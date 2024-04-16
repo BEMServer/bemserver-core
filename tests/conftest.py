4b25fa51b25c939058f884fdf494f4fe6da250f3
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2024-04-16 11:21:36 +0200

    Minor pyproject.toml fix

--- tests/conftest.py
+++ tests/conftest.py
@@ -4,7 +4,6 @@ import datetime as dt
 from unittest import mock
 
 import pytest
-
 from pytest_postgresql import factories as ppf
 
 from bemserver_core import BEMServerCore, model, scheduled_tasks
