0d9919bedd35a7be34cd7c9fe9511b6f997f103f
Sébastien Celles <scls19fr@users.noreply.github.com>
2024-04-22 16:13:47 +0200

    Update bemserver-celery-settings.py (#233)
    * Update bemserver-celery-settings.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- docs/deployment/srv/bemserver-core/application/bemserver-celery-settings.py
+++ docs/deployment/srv/bemserver-core/application/bemserver-celery-settings.py
@@ -8,9 +8,9 @@ beat_schedule = {
     },
     "check_missing": {
         "task": "CheckMissing",
-        "schedule": crontab(minute="0", hour="2", day="*"),
+        "schedule": crontab(minute="0", hour="2", day_of_month="*"),
         "kwargs": {
-            "timezone": "Europe/Paris",
+            "timezone": timezone,
             "min_completeness_ratio": 0.5,
             "period": "day",
             "period_multiplier": 1,
@@ -18,9 +18,9 @@ beat_schedule = {
     },
     "check_outliers": {
         "task": "CheckOutliers",
-        "schedule": crontab(minute="0", hour="2", day="*"),
+        "schedule": crontab(minute="0", hour="2", day_of_month="*"),
         "kwargs": {
-            "timezone": "Europe/Paris",
+            "timezone": timezone,
             "min_correctness_ratio": 0.5,
             "period": "day",
             "period_multiplier": 1,
