1abc32d2951c7557894876dca3d1b069623a06d7
Sébastien Celles <scls19fr@users.noreply.github.com>
2024-04-22 09:49:11 +0200

    Update bemserver-celery-settings.py

--- docs/deployment/srv/bemserver-core/application/bemserver-celery-settings.py
+++ docs/deployment/srv/bemserver-core/application/bemserver-celery-settings.py
@@ -1,5 +1,6 @@
 from celery.schedules import crontab
 
+
 timezone = "Europe/Paris"
 beat_schedule = {
     "cleanup": {
@@ -8,9 +9,9 @@ beat_schedule = {
     },
     "check_missing": {
         "task": "CheckMissing",
-        "schedule": crontab(minute="0", hour="2", day="*"),
+        "schedule": crontab(minute="0", hour="2", day_of_month="*"),
         "kwargs": {
-            "timezone": "Europe/Paris",
+            "timezone": timezone,
             "min_completeness_ratio": 0.5,
             "period": "day",
             "period_multiplier": 1,
@@ -18,9 +19,9 @@ beat_schedule = {
     },
     "check_outliers": {
         "task": "CheckOutliers",
-        "schedule": crontab(minute="0", hour="2", day="*"),
+        "schedule": crontab(minute="0", hour="2", day_of_month="*"),
         "kwargs": {
-            "timezone": "Europe/Paris",
+            "timezone": timezone,
             "min_correctness_ratio": 0.5,
             "period": "day",
             "period_multiplier": 1,
