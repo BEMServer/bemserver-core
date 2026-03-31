7ef4852cf0f5625e3c6f8417c266e9bf4c1de6e2
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2026-03-31 19:10:39 +0200

    Rework authorizations: tasks

--- src/bemserver_core/model/tasks.py
+++ src/bemserver_core/model/tasks.py
@@ -6,13 +6,15 @@ from zoneinfo import ZoneInfo
 import sqlalchemy as sqla
 from sqlalchemy.dialects.postgresql import JSONB
 
-from bemserver_core.authorization import AuthMixin, Relation, auth
+from bemserver_core.authorization import AuthMgrMixin, Relation
 from bemserver_core.celery import logger
 from bemserver_core.database import Base
 from bemserver_core.time_utils import PeriodEnum, make_date_offset
 
+from .campaigns import Campaign
 
-class TaskByCampaign(AuthMixin, Base):
+
+class TaskByCampaign(AuthMgrMixin, Base):
     __tablename__ = "tasks_by_campaigns"
 
     id = sqla.Column(sqla.Integer, primary_key=True)
@@ -33,8 +35,7 @@ class TaskByCampaign(AuthMixin, Base):
 
     @classmethod
     def register_class(cls):
-        auth.register_class(
-            cls,
+        super().register_class(
             fields={
                 "campaign": Relation(
                     kind="one",
@@ -44,6 +45,13 @@ class TaskByCampaign(AuthMixin, Base):
                 ),
             },
         )
+
+    @classmethod
+    def authorize_query(cls, actor, query):
+        return Campaign.authorize_query(actor, query.join(Campaign))
+
+    def authorize_read(self, actor):
+        return self.campaign.is_member(actor)
 
     def make_interval(self):
         datetime = dt.datetime.now(tz=ZoneInfo(self.campaign.timezone))
