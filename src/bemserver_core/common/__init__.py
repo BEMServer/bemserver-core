c688beab0fe8b3cbde9688e9c69c66ab09ed8d8c
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2026-04-21 17:00:23 +0200

    Add AggregationFunctionsEnum

--- src/bemserver_core/common/__init__.py
+++ src/bemserver_core/common/__init__.py
@@ -1,4 +1,14 @@
 """Common"""
 
+import enum
+
 from .property_type import PropertyType  # noqa
 from .units import ureg  # noqa
+
+
+class AggregationFunctionsEnum(enum.StrEnum):
+    AVG = "avg"
+    SUM = "sum"
+    MIN = "min"
+    MAX = "max"
+    COUNT = "count"
