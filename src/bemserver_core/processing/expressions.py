00870bd68941b6ea5d8ada120893352a4b4dd67e
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2026-06-18 16:53:02 +0200

    Add get_expression_variable_values

--- src/bemserver_core/processing/expressions.py
+++ src/bemserver_core/processing/expressions.py
@@ -10,6 +10,29 @@ from bemserver_core.input_output import tsdio
 from bemserver_core.model import TimeseriesDataState
 
 
+def get_expression_variable_values(
+    expression_variable,
+    start_dt,
+    end_dt,
+    data_state,
+    bucket_width_value,
+    bucket_width_unit,
+    timezone="UTC",
+):
+    return tsdio.get_timeseries_buckets_data(
+        start_dt,
+        end_dt,
+        [expression_variable.timeseries],
+        data_state,
+        bucket_width_value,
+        bucket_width_unit,
+        aggregation=expression_variable.aggregation,
+        convert_to=expression_variable.unit_symbol,
+        timezone=timezone,
+        col_label="id",
+    )[expression_variable.timeseries_id]
+
+
 def evaluate(
     expression,
     start_dt,
@@ -23,18 +46,15 @@ def evaluate(
     ds_clean = TimeseriesDataState.get(name="Clean").first()
 
     for expr_var in expression.variables:
-        namespace[expr_var.name] = tsdio.get_timeseries_buckets_data(
+        namespace[expr_var.name] = get_expression_variable_values(
+            expr_var,
             start_dt,
             end_dt,
-            [expr_var.timeseries],
             ds_clean,
             bucket_width_value,
             bucket_width_unit,
-            aggregation=expr_var.aggregation,
-            convert_to=expr_var.unit_symbol,
             timezone=timezone,
-            col_label="id",
-        )[expr_var.timeseries_id]
+        )
 
     data_s = expression_eval.evaluate(expression.expr, namespace)
     data_df = pd.DataFrame({expression.timeseries_id: data_s}, data_s.index)
--- tests/processing/test_expressions.py
+++ tests/processing/test_expressions.py
@@ -5,17 +5,72 @@ import datetime as dt
 import pytest
 
 import pandas as pd
-from pandas.testing import assert_frame_equal
+from pandas.testing import assert_frame_equal, assert_series_equal
 
 from bemserver_core.authorization import CurrentUser, OpenBar
 from bemserver_core.database import db
 from bemserver_core.input_output import tsdio
 from bemserver_core.model import Expression, ExpressionVariable, TimeseriesDataState
-from bemserver_core.processing.expressions import evaluate
+from bemserver_core.processing.expressions import (
+    evaluate,
+    get_expression_variable_values,
+)
 from tests.utils import create_timeseries_data
 
 
 class TestExpressionsEvaluateProcessing:
+    @pytest.mark.parametrize("campaigns", (2,), indirect=True)
+    @pytest.mark.parametrize("timeseries", (3,), indirect=True)
+    def test_get_expression_variable_values(self, users, timeseries, campaign_scopes):
+        admin_user = users[0]
+        assert admin_user.is_admin
+        cs_1 = campaign_scopes[0]
+        ts_1 = timeseries[0]
+        ts_3 = timeseries[2]
+
+        with OpenBar():
+            ds_clean = TimeseriesDataState.get(name="Clean").first()
+            expr_1 = Expression.new(
+                campaign_scope_id=cs_1.id,
+                expr="2*a",
+                timeseries_id=ts_1.id,
+            )
+            db.session.flush()
+            expr_var_1 = ExpressionVariable.new(
+                campaign_scope_id=cs_1.id,
+                expression_id=expr_1.id,
+                name="a",
+                timeseries_id=ts_3.id,
+                aggregation="avg",
+            )
+            db.session.flush()
+
+        start_dt = dt.datetime(2020, 1, 1, tzinfo=dt.UTC)
+        end_dt = dt.datetime(2020, 1, 2, tzinfo=dt.UTC)
+        timestamps = pd.date_range(start_dt, end_dt, inclusive="left", freq="6h")
+        values_2 = [0, 2, 4, 8]
+        create_timeseries_data(ts_3, ds_clean, timestamps, values_2)
+
+        with CurrentUser(admin_user):
+            data_s = get_expression_variable_values(
+                expr_var_1,
+                start_dt,
+                end_dt,
+                ds_clean,
+                6,
+                "hour",
+                timezone="UTC",
+            )
+            expected_s = pd.Series(
+                [0, 2, 4, 8],
+                index=pd.DatetimeIndex(timestamps, name="timestamp", freq="6h").as_unit(
+                    "us"
+                ),
+                name=ts_3.id,
+                dtype=float,
+            )
+            assert_series_equal(data_s, expected_s)
+
     @pytest.mark.parametrize("campaigns", (2,), indirect=True)
     @pytest.mark.parametrize("timeseries", (3,), indirect=True)
     def test_evaluate(self, users, timeseries, campaign_scopes):
