2774099e6e95dac10336f460c4029b635752bafc
Jérôme Lafréchoux <jlafrechoux@nobatek.inef4.com>
2026-06-26 16:49:31 +0200

    Add expressions.evaluate_from_dict

--- src/bemserver_core/processing/expressions.py
+++ src/bemserver_core/processing/expressions.py
@@ -5,6 +5,7 @@ Evaluate expressions
 
 from bemserver_core import expression_eval
 from bemserver_core.input_output import tsdio
+from bemserver_core.model import Timeseries
 
 
 def get_expression_variable_values(
@@ -53,3 +54,34 @@ def evaluate(
         )
 
     return expression_eval.evaluate(expression.expr, namespace).rename(expression.name)
+
+
+def evaluate_from_dict(
+    expression,
+    start_dt,
+    end_dt,
+    data_state,
+    bucket_width_value,
+    bucket_width_unit,
+    timezone="UTC",
+):
+    namespace = {}
+
+    for expr_var in expression["variables"]:
+        timeseries = Timeseries.get_by_id(expr_var["timeseries_id"])
+        namespace[expr_var["name"]] = tsdio.get_timeseries_buckets_data(
+            start_dt,
+            end_dt,
+            [timeseries],
+            data_state,
+            bucket_width_value,
+            bucket_width_unit,
+            aggregation=expr_var["aggregation"],
+            convert_to=expr_var["unit_symbol"],
+            timezone=timezone,
+            col_label="id",
+        )[expr_var["timeseries_id"]]
+
+    return expression_eval.evaluate(expression["expr"], namespace).rename(
+        expression["name"]
+    )
--- tests/processing/test_expressions.py
+++ tests/processing/test_expressions.py
@@ -10,6 +10,7 @@ from bemserver_core.database import db
 from bemserver_core.model import Expression, ExpressionVariable, TimeseriesDataState
 from bemserver_core.processing.expressions import (
     evaluate,
+    evaluate_from_dict,
     get_expression_variable_values,
 )
 from tests.utils import create_timeseries_data
@@ -126,6 +127,85 @@ class TestExpressionsEvaluateProcessing:
             assert_series_equal(data_s, expected_s)
 
             data_s = evaluate(expr_1, start_dt, end_dt, ds_clean, 12, "hour")
+            expected_s = pd.Series(
+                [2, 12],
+                pd.DatetimeIndex(
+                    pd.date_range(start_dt, end_dt, inclusive="left", freq="12h"),
+                    name="timestamp",
+                    freq="12h",
+                ).as_unit("us"),
+                name="2 times a",
+                dtype=float,
+            )
+            assert_series_equal(data_s, expected_s)
+
+    def test_evaluate_from_dict(self, users, timeseries, campaign_scopes):
+        admin_user = users[0]
+        assert admin_user.is_admin
+        cs_1 = campaign_scopes[0]
+        ts_1 = timeseries[0]
+
+        with OpenBar():
+            ds_clean = TimeseriesDataState.get(name="Clean").first()
+            expr_1 = Expression.new(
+                campaign_scope_id=cs_1.id,
+                name="2 times a",
+                expr="2*a",
+            )
+            expr_2 = Expression.new(
+                campaign_scope_id=cs_1.id,
+                name="a squared",
+                expr="a**2",
+            )
+            db.session.flush()
+            ExpressionVariable.new(
+                campaign_scope_id=cs_1.id,
+                expression_id=expr_1.id,
+                name="a",
+                timeseries_id=ts_1.id,
+                aggregation="avg",
+            )
+            ExpressionVariable.new(
+                campaign_scope_id=cs_1.id,
+                expression_id=expr_2.id,
+                name="a",
+                timeseries_id=ts_1.id,
+                aggregation="avg",
+            )
+            db.session.flush()
+
+        start_dt = dt.datetime(2020, 1, 1, tzinfo=dt.UTC)
+        end_dt = dt.datetime(2020, 1, 2, tzinfo=dt.UTC)
+        timestamps = pd.date_range(start_dt, end_dt, inclusive="left", freq="6h")
+        values_2 = [0, 2, 4, 8]
+        create_timeseries_data(ts_1, ds_clean, timestamps, values_2)
+
+        with CurrentUser(admin_user):
+            data_s = evaluate_from_dict(
+                expr_1.to_dict(), start_dt, end_dt, ds_clean, 6, "hour"
+            )
+            expected_s = pd.Series(
+                [0, 4, 8, 16],
+                pd.DatetimeIndex(timestamps, name="timestamp", freq="6h").as_unit("us"),
+                name="2 times a",
+                dtype=float,
+            )
+            assert_series_equal(data_s, expected_s)
+
+            data_s = evaluate_from_dict(
+                expr_2.to_dict(), start_dt, end_dt, ds_clean, 6, "hour"
+            )
+            expected_s = pd.Series(
+                [0, 4, 16, 64],
+                pd.DatetimeIndex(timestamps, name="timestamp", freq="6h").as_unit("us"),
+                name="a squared",
+                dtype=float,
+            )
+            assert_series_equal(data_s, expected_s)
+
+            data_s = evaluate_from_dict(
+                expr_1.to_dict(), start_dt, end_dt, ds_clean, 12, "hour"
+            )
             expected_s = pd.Series(
                 [2, 12],
                 pd.DatetimeIndex(
